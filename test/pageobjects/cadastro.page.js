7e0c73fd79b1596702d51384cd9328d6a324f7a1
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2026-02-13 16:53:31 -0300

    readme atualizado2

--- test/pageobjects/cadastro.page.js
+++ test/pageobjects/cadastro.page.js
@@ -1,20 +1,54 @@
 class CadastroPage {
     // Seletores
     get menuLogin() { return $('~Login') }
-    get menuSignUp() { return $('~Sign up') }
+    get menuSignUp() { 
+        if (driver.isAndroid) {
+            return $('//android.widget.TextView[@text="Sign up"]')
+        }
+        return $('~Sign up') 
+    }
     get campoEmail() { return $('~input-email') }
     get campoSenha() { return $('~input-password') }
     get campoConfirmaSenha() { return $('~input-repeat-password') }
-    get btnCadastrar() { return $('~SIGN UP') }
-    get msgSucesso () { return $('~Signed Up!')}
-    get msgErroEmail () {return $('~Please enter a valid email address')}
-    get msgErroConfirmaSenha () {return $('~Please enter the same password')}
-    get msgErroSenha () {return $('~Please enter at least 8 characters')}
+    get btnCadastrar() { 
+        if (driver.isAndroid) {
+            return $('//android.widget.TextView[@text="SIGN UP"]')
+        } else {    
+            return $('~SIGN UP') }
+    }
+    get msgSucesso () { 
+        if (driver.isAndroid) {
+            return $('//android.widget.TextView[@resource-id="android:id/alertTitle"]')
+        } else {
+        return $('~Signed Up!')}
+        
+    }
+    get msgErroEmail () {
+        if (driver.isAndroid) {
+            return $('//android.widget.TextView[@text="Please enter a valid email address"]')
+        } else {        
+                return $('~Please enter a valid email address')}
+    }
+    get msgErroConfirmaSenha () {
+        if (driver.isAndroid) {
+            return $('//android.widget.TextView[@text="Please enter the same password"]')
+        } else {    
+        return $('~Please enter the same password')}
+    }
+    get msgErroSenha () {   
+        if (driver.isAndroid) {
+            return $('//android.widget.TextView[@text="Please enter at least 8 characters"]')
+        } else {
+            return $('~Please enter at least 8 characters')
+        }
+    }
 
     // Ações
     async open() {
         await this.menuLogin.click()
+        console.log('Clicou no menu Login')
         await this.menuSignUp.click()
+        console.log('Clicou no menu Sign Up')
     }
 
     async preencherFormulario(email, senha, confirmarSenha) {
@@ -23,10 +57,10 @@ class CadastroPage {
         await this.campoEmail.addValue(email) //Outra forma de preencher 
 
         await this.campoSenha.clearValue()
-        await this.campoSenha.setValue(senha)
+        await this.campoSenha.addValue(senha)
 
         await this.campoConfirmaSenha.clearValue()
-        await this.campoConfirmaSenha.setValue(confirmarSenha)
+        await this.campoConfirmaSenha.addValue(confirmarSenha)
 
         await this.btnCadastrar.click()
     }
